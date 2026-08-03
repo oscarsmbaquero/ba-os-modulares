5f415b924e0b6cf2d9142ed26630bcbcef8cd758
oscarsmbaquero <oscarsmbaquero@gmail.com>
2026-08-03 13:43:53 +0200

    ajustes footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { CookieService } from '../../services/cookie';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, MatIconModule],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
